da038e5a0d9fbd35d9e8cbafd769a51bae68579a
Olga-Zyukina <bloger_80@mail.ru>
2026-09-06 14:50:40 +0300

    update  packages

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
